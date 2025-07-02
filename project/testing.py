3dd3dad6824f0c6e13631ecfd82c35e7268a2b08
Ishaanv1905 <ishaanv19@gmail.com>
2025-07-02 16:41:31 +0530

    adding

---
+++ project/testing.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+
+# Load model
+model = YOLO("yolo10n.pt")
+
+# Run inference on folder of images
+results = model.predict(
+    source=r"C:\Users\Ishaan Verma\Desktop\ACT\VLM-Safety Gear Detection\project\test_images",
+    imgsz=640,
+    conf=0.25
+)
+
+# Visualize results
+for r in results:
+    r.show()
